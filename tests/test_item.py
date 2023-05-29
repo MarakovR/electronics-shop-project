c79c7af254eca5a333ee26d287c3b21ee11dca6f
Roman Marakov <marakofr76@yandex.ru>
2023-05-29 21:51:23 +0300

    Выполнение задания №1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+test_item = Item("ПК", 50000, 3)
+def test_calculate_total_price():
+    assert test_item.calculate_total_price() == 150000
+
+def test_apply_discount():
+    test_item.apply_discount()
+    assert test_item.price == 50000
+    Item.pay_rate = 0.2
+    test_item.apply_discount()
+    assert test_item.price == 10000
