a55bc861e3cbca663bd7260c347855db72698a23
Roman Marakov <marakofr76@yandex.ru>
2023-06-08 18:26:12 +0300

    Выполнение задания №3, доработка задания №2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,5 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import pytest
 
 test_item = Item("ПК", 50000, 3)
 
@@ -19,11 +20,16 @@ def test_apply_discount():
 def test_name():
     item = Item('Смартфон', 10000, 5)
     assert item.name == 'Смартфон'
-    item.name = 'СуперСмартфон'
-    assert item.name == 'Смартфон'
+    with pytest.raises(ValueError):
+        item.name = 'СуперСмартфон'
 
 
 def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.5') == 5
+
+def test_repr_and_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+    assert str(item1) == 'Смартфон'
